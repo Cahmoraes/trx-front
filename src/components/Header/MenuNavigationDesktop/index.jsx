47d4430d1736ab1b9470ff33f279b0f59af48410
caique.moraes <ike.moraes93@gmail.com>
2021-07-29 19:53:19 -0300

    header e menu

---
+++ src/components/Header/MenuNavigationDesktop/index.jsx
@@ -0,0 +1,44 @@
+export function MenuNavigation() {
+  return (
+    <nav className="col-12 c-menu-navigation">
+      <ul>
+        <li>
+          <a href="/">Escritório</a>
+        </li>
+        <li>
+          <a href="/">Home Office</a>
+        </li>
+        <li>
+          <a href="/">Ferramentas</a>
+        </li>
+        <li>
+          <a href="/">Geek</a>
+        </li>
+        <li>
+          <a href="/">Moda</a>
+        </li>
+        <li>
+          <a href="/">Móveis e Decoração</a>
+        </li>
+        <li>
+          <a href="/">Celulares</a>
+        </li>
+        <li>
+          <a href="/">Eletrônicos</a>
+        </li>
+        <li>
+          <a href="/">Splits e Ar-condicionado</a>
+        </li>
+        <li>
+          <a href="/">Pets</a>
+        </li>
+        <li>
+          <a href="/">Promoções</a>
+        </li>
+        <li>
+          <a href="/">MAIS</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,32 +1,57 @@
-import { Search } from './Search';
-import logoSvg from '../../assets/images/icons/logo.svg';
-import { ReactComponent as UserIcon } from '../../assets/images/icons/user-icon.svg';
-import { ReactComponent as CartIcon } from '../../assets/images/icons/cart-icon.svg';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { Search } from './Search'
+import { MenuNavigation } from './MenuNavigationDesktop'
+
+import logoSvg from '../../assets/images/icons/logo.svg'
+import { ReactComponent as UserIcon } from '../../assets/images/icons/user-icon.svg'
+import { ReactComponent as CartIcon } from '../../assets/images/icons/cart-icon.svg'
+import { ReactComponent as MenuMobileIcon } from '../../assets/images/icons/menu-mobile.svg'
 
 export function Header() {
   return (
-    <header className="l-header">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-3 l-header__logo">
-            <Link to="/home">
-              <img src={logoSvg} alt="TRX" />
-            </Link>
-          </div>
-          <div className="col-6 l-header__search">
-            <Search />
-          </div>
-          <div className="col-3 l-header__user-menu">
-            <button>
-              <UserIcon />
-            </button>
-            <button>
-              <CartIcon />
-            </button>
+    <>
+      <header className="l-header">
+        <div className="container">
+          <div className="row align-items-center justify-content-between">
+
+            <div className="col-1 l-header__menu-navigation-mobile">
+              <button>
+                <MenuMobileIcon />
+              </button>
+            </div>
+
+            <div className="col-3 l-header__logo">
+              <Link to="/home">
+                <img src={logoSvg} alt="TRX" />
+              </Link>
+            </div>
+
+            <div className="col-1 col-md-6 l-header__search-desk">
+              <Search />
+            </div>
+
+            <div className="col-1 col-md-3 l-header__user-menu">
+              <div className="l-header__search-mobile">
+                <Search />
+              </div>
+
+              <button className="l-header__userIcon">
+                <UserIcon />
+              </button>
+              <button className="l-header__cartIcon">
+                <CartIcon />
+              </button>
+            </div>
+
           </div>
         </div>
+      </header>
+
+      <div className="container-fluid l-header__menu-navigation-desk">
+        <div className="row">
+          <MenuNavigation />
+        </div>
       </div>
-    </header>
+    </>
   )
 }
