12aaafead932dbb630a6f6a0bfc0da8d58737a97
caique.moraes <ike.moraes93@gmail.com>
2021-08-02 09:46:46 -0300

    hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,18 +1,32 @@
 import { SliderArrow } from '../SliderArrow'
 import bannerJPG from '../../assets/images/banner.jpg'
+import bannerMobileJPG from '../../assets/images/banner-mobile.jpg'
 
 export function Hero() {
   return (
     <div className="l-hero">
-      
-      <div className="l-hero__banner">
-        <img src={bannerJPG} alt="Banner" />
-      </div>
+
+      <picture className="l-hero__banner">
+        <source media="(min-width:650px)" srcSet={bannerJPG} />
+        <source media="(min-width:0)" srcSet={bannerMobileJPG} />
+        <img src={bannerJPG} alt={bannerJPG} />
+      </picture>
 
       <div className="l-hero__arrow-container">
         <SliderArrow />
         <SliderArrow sense="right" />
       </div>
+
+      <div className="l-hero__control-container">
+        <ul>
+          <li><a href="/">&nbsp;</a></li>
+          <li><a href="/">&nbsp;</a></li>
+          <li><a href="/">&nbsp;</a></li>
+          <li><a href="/">&nbsp;</a></li>
+          <li><a href="/">&nbsp;</a></li>
+        </ul>
+      </div>
+
     </div>
   )
 }
