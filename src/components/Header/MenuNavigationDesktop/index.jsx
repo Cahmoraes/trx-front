f348561483f7095f5c60d35a60c5e896bbc53d9c
Cahmoraes <ike.moraes93@gmail.com>
2021-08-05 12:19:12 -0300

    menu open

--- src/components/Header/MenuNavigationDesktop/index.jsx
+++ src/components/Header/MenuNavigationDesktop/index.jsx
@@ -1,7 +1,7 @@
 export function MenuNavigation() {
   return (
     <nav className="col-12 c-menu-navigation">
-      <ul>
+      <ul className="c-menu-navigation__top-list">
         <li>
           <a href="/">Escritório</a>
         </li>
@@ -39,6 +39,59 @@ export function MenuNavigation() {
           <a href="/">MAIS</a>
         </li>
       </ul>
+      <div className="c-menu-navigation__content">
+        <div className="c-menu-navigation__box">
+          <h3 className="c-menu-navigation__box-title">Informática</h3>
+          <ul className="c-menu-navigation__bottom-list">
+            <li className="c-menu-navigation__box-item"><a href="/">Notebook</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Notebook Gamer</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">PC Gamer</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Computador</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Monitor</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Tablet</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Impressora</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Projetor</a></li>
+          </ul>
+        </div>
+
+        <div className="c-menu-navigation__box">
+          <h3 className="c-menu-navigation__box-title">Acessórios</h3>
+          <ul className="c-menu-navigation__bottom-list">
+            <li className="c-menu-navigation__box-item"><a href="/">Capa para notebook</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Mouse</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Teclado</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Pen drive</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">HD externo</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Adaptador</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Fone de ouvido</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Headset</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Mochila</a></li>
+          </ul>
+        </div>
+
+        <div className="c-menu-navigation__box">
+          <h3 className="c-menu-navigation__box-title">Eletroportáteis</h3>
+          <ul className="c-menu-navigation__bottom-list">
+            <li className="c-menu-navigation__box-item"><a href="/">Smart Home</a></li>
+            <li className="c-menu-navigation__box-item c-menu-navigation__box-item--active"><a href="/">Cafeteira</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Torradeira</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Aquecedor</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Umidificador de ar</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Ar-condicionado</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Ventilador</a></li>
+          </ul>
+        </div>
+
+        <div className="c-menu-navigation__box">
+          <h3 className="c-menu-navigation__box-title">Cadeiras Giratórias</h3>
+          <ul className="c-menu-navigation__bottom-list">
+            <li className="c-menu-navigation__box-item"><a href="/">Escrivaninhas</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Organizadores</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Kits</a></li>
+            <li className="c-menu-navigation__box-item"><a href="/">Papelarias</a></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
