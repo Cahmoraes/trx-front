b2a50c9e2b78432411c352453c950ec24b125fca
Cahmoraes <ike.moraes93@gmail.com>
2021-08-16 11:14:33 -0300

    enhancements

--- src/pages/ProductDetails/ProductGallery/index.jsx
+++ src/pages/ProductDetails/ProductGallery/index.jsx
@@ -5,32 +5,32 @@ import { ReactComponent as ArrowRightIcon } from '../../../assets/images/icons/a
 export function ProductGallery({ productImages, setGalleryState }) {
   if (!productImages.length) return null
   return (
-    <div className="l-product-gallery">
-      <div className="l-product-gallery__container">
-        <header className="l-product-gallery__header">
-          <button className="l-product-gallery__close" onClick={() => setGalleryState(false)}>
+    <div className="c-product-gallery">
+      <div className="c-product-gallery__container">
+        <header className="c-product-gallery__header">
+          <button className="c-product-gallery__close" onClick={() => setGalleryState(false)}>
             <CloseIcon />
           </button>
         </header>
 
-        <main className="l-product-gallery__main">
-          <p className="l-product-gallery__slide-position">1 de 8</p>
+        <main className="c-product-gallery__main">
+          <p className="c-product-gallery__slide-position">1 de 8</p>
           <img src={productImages[0].src} alt={productImages[0].description} />
         </main>
 
-        <footer className="l-product-gallery__footer">
-          <button className="l-product-gallery__arrow"><ArrowLeftIcon /></button>
+        <footer className="c-product-gallery__footer">
+          <button className="c-product-gallery__arrow"><ArrowLeftIcon /></button>
           {
             productImages.slice(1).map((image, index) => (
               index === 0 ? (
-                <button key={image.id} className="l-product-gallery__item l-product-gallery__item--active">
+                <button key={image.id} className="c-product-gallery__item c-product-gallery__item--active">
                   <img
                     src={image.src}
                     alt={image.description}
                   />
                 </button>
               ) : (
-                <button key={image.id} className="l-product-gallery__item">
+                <button key={image.id} className="c-product-gallery__item">
                   <img
                     key={image.src}
                     src={image.src}
@@ -40,7 +40,7 @@ export function ProductGallery({ productImages, setGalleryState }) {
               )
             ))
           }
-          <button className="l-product-gallery__arrow"><ArrowRightIcon /></button>
+          <button className="c-product-gallery__arrow"><ArrowRightIcon /></button>
         </footer>
       </div>
     </div>
